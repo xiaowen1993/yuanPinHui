89773244227de8a64c07a83cbace492a1b354c31
LC <2859257083@qq.com>
2020-11-12 17:43:10 +0800

    system

--- system/src/main/java/com/yph/modules/system/controller/AdminController.java
+++ system/src/main/java/com/yph/modules/system/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-12
  */
 @RestController
-@RequestMapping("/system/admin-entity")
+@RequestMapping("/system/admin")
 public class AdminController {
 
 }
