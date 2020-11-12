e310c85922b82c8b12a50a57654c7422d9e141a3
tjx <123456>
2020-11-12 15:06:12 +0800

    项目初始化

---
+++ tool/src/main/java/com.yph/util/EmptyUtil.java
@@ -0,0 +1,41 @@
+package com.yph.util;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author  nachuan
+ * 判断 String int  Collection 是否为空
+ */
+public class EmptyUtil implements Serializable {
+    /**
+     * 如果参数为null，返回true
+     * * @author hechenglong
+     * @param patterns
+     * @return
+     */
+    public static boolean isNull(Object... patterns){
+        if(patterns == null || patterns.length == 0) return true;
+        for (Object obj : patterns) {
+            if(obj == null || "".equals(obj) || "".equals(obj.toString().trim()) || "null".equalsIgnoreCase(obj.toString())){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 如果参数不为null，返回true
+     * @author hechenglong
+     * @param patterns
+     * @return
+     */
+    public static boolean notNull(Object... patterns) {
+        return !isNull(patterns);
+    }
+
+
+
+}
