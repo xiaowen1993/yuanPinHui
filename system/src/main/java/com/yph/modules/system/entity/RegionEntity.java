14b6b4758c34a6ae3f4c4d4da7d8d4f66af84872
ly <1293876738@qq.com>
2020-11-17 14:37:05 +0800

    region

--- system/src/main/java/com/yph/modules/system/entity/RegionEntity.java
+++ system/src/main/java/com/yph/modules/system/entity/RegionEntity.java
@@ -18,78 +18,62 @@ public class RegionEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(value = "id", type = IdType.AUTO)
-    private Integer id;
-
-    /**
-     * 行政区域父ID，例如区县的pid指向市，市的pid指向省，省的pid则是0
-     */
-    private Integer pid;
+    private Integer areaId;
 
     /**
      * 行政区域名称
      */
-    private String name;
+    private String areaName;
+
+    /**
+     * 行政区域父ID，例如区县的pid指向市，市的pid指向省，省的pid则是0
+     */
+    private Integer parentId;
 
     /**
      * 行政区域类型，如如1则是省， 如果是2则是市，如果是3则是区县
      */
-    private Integer type;
+    private Integer level;
 
-    /**
-     * 行政区域编码
-     */
-    private Integer code;
-
-    public Integer getId() {
-        return id;
+    public Integer getAreaId() {
+        return areaId;
     }
 
-    public RegionEntity setId(Integer id) {
-        this.id = id;
-        return this;
-    }
-    public Integer getPid() {
-        return pid;
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
     }
 
-    public RegionEntity setPid(Integer pid) {
-        this.pid = pid;
-        return this;
-    }
-    public String getName() {
-        return name;
+    public String getAreaName() {
+        return areaName;
     }
 
-    public RegionEntity setName(String name) {
-        this.name = name;
-        return this;
-    }
-    public Integer getType() {
-        return type;
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
     }
 
-    public RegionEntity setType(Integer type) {
-        this.type = type;
-        return this;
-    }
-    public Integer getCode() {
-        return code;
+    public Integer getParentId() {
+        return parentId;
     }
 
-    public RegionEntity setCode(Integer code) {
-        this.code = code;
-        return this;
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
     }
 
     @Override
     public String toString() {
         return "RegionEntity{" +
-            "id=" + id +
-            ", pid=" + pid +
-            ", name=" + name +
-            ", type=" + type +
-            ", code=" + code +
-        "}";
+                "areaId=" + areaId +
+                ", areaName='" + areaName + '\'' +
+                ", parentId=" + parentId +
+                ", level=" + level +
+                '}';
     }
 }
